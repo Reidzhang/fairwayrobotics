95e5b871fcb30719fdce5a72aece61e869972423
Reidzhang <zzt124@cs.washington.edu>
2016-04-30 12:39:23 -0700

    homming and dance

--- scripts/roomba.py
+++ scripts/roomba.py
@@ -29,33 +29,35 @@ CTRL-C to quit
 class Roomba:
 
     def __init__(self):
-	self.twist = Twist()
+	    self.twist = Twist()
         self.bump_sub = rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, self.processBump)
         self.pub = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=5)
-	self.lock = False
+	    self.lock = False
         self.moveForward()
 
     def processBump(self, msg):
         if msg.state == BumperEvent.PRESSED:
-	    self.lock = True
-	    for k in range(0, 10):
-	        self.twist.angular.z = 0
+	        print "True"
+            self.lock = True
+	        for k in range(0, 10):
+	            self.twist.angular.z = 0
                 self.twist.linear.x = -0.1
                 self.pub.publish(self.twist)
                 time.sleep(0.1)
 
-	    sign = randint(-1,1)
+	        sign = randint(-1,1)
 	    
             for k in range(0, 25):
-		self.twist.angular.z = sign * 0.75
+		        self.twist.angular.z = sign * 0.75
                 self.twist.linear.x = 0
                 self.pub.publish(self.twist)
-		time.sleep(0.1)
+		        time.sleep(0.1)
 	    
 	    self.lock = False
             
 
     def moveForward(self):
+        print "hi"
         while(1):
             if (not self.lock):
 	    	self.twist.angular.z = 0
